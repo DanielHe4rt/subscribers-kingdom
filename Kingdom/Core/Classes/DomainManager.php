bd09874890dbfec8068df63cb44f0f1609fe1019
danielhe4rt <hey@danielheart.dev>
2023-01-01 18:32:43 -0300

    feat: domain with subdomain

--- Kingdom/Core/Classes/DomainManager.php
+++ Kingdom/Core/Classes/DomainManager.php
@@ -2,25 +2,31 @@
 
 namespace Kingdom\Core\Classes;
 
+use FilesystemIterator;
 use Kingdom\Core\Contracts\DomainInterface;
 use Kingdom\Core\Exceptions\DomainNotExistsException;
 use Kingdom\Core\Exceptions\DomainExtendException;
+use Kingdom\Core\Traits\Singleton;
 use RecursiveIteratorIterator;
 use Illuminate\Support\Facades\File;
 use RecursiveDirectoryIterator;
 use Illuminate\Support\Str;
 
 
-class DomainManager {
+class DomainManager
+{
 
-    use \Kingdom\Core\Traits\Singleton;
+    use Singleton;
 
     public array $domains = [];
-    public bool $booted = false;
-    public bool $registered = false;
-    protected array $normalizedMap;
-    protected array $pathMap;
 
+    public bool $booted = false;
+
+    public bool $registered = false;
+
+    protected array $normalizedMap;
+
+    protected array $pathMap;
 
     /**
      * @codeCoverageIgnore
@@ -53,7 +59,7 @@ class DomainManager {
 
         foreach ($this->getVendorAndDomainNames() as $vendorName => $vendorList) {
             foreach ($vendorList as $domainName => $domainPath) {
-                $namespace = '\\'.$vendorName.'\\'.$domainName;
+                $namespace = '\\' . $vendorName . '\\' . $domainName;
                 $namespace = $this->normalizeClassName($namespace);
                 $classNames[$namespace] = $domainPath;
             }
@@ -68,7 +74,7 @@ class DomainManager {
             $name = get_class($name);
         }
 
-        return '\\'.ltrim($name, '\\');
+        return '\\' . ltrim($name, '\\');
     }
 
     /**
@@ -78,25 +84,27 @@ class DomainManager {
      */
     public function getVendorAndDomainNames(): array
     {
-        $dir = __DIR__ . "/../../";
+        $dir = __DIR__ . '/../../';
         $domains = [];
         $dirPath = realpath($dir);
+        $dirPathSeparated = explode('/', $dirPath);
+        $coreDomain = array_pop($dirPathSeparated);
         if (!File::isDirectory($dirPath)) {
             return $domains;
         }
 
-
         $it = new RecursiveIteratorIterator(
-            new RecursiveDirectoryIterator($dirPath, RecursiveDirectoryIterator::FOLLOW_SYMLINKS)
+            new RecursiveDirectoryIterator($dirPath, FilesystemIterator::FOLLOW_SYMLINKS)
         );
-        $it->setMaxDepth(1);
+        $it->setMaxDepth();
         $it->rewind();
 
         while ($it->valid()) {
-            if ($it->isFile() && (strtolower($it->getFilename()) == "domain.php")) {
+            if ($it->isFile() && (strtolower($it->getFilename()) == 'domain.php')) {
                 $filePath = dirname($it->getPathname());
+                $vendorPaths = array_reverse(explode('/', $filePath));
                 $domainName = basename($filePath);
-                $vendorName = basename(dirname($filePath));
+                $vendorName = $this->getPathFromCoreDomain($coreDomain, $vendorPaths);
                 $domains[$vendorName][$domainName] = $filePath;
             }
 
@@ -107,11 +115,34 @@ class DomainManager {
     }
 
     /**
+     * Returns a path to a subdomain from the core domain
+     *
+     * @param string $coreDomain
+     * @param array $vendorPaths
+     * @return string
+     */
+    private function getPathFromCoreDomain(string $coreDomain, array $vendorPaths): string
+    {
+        $vendorName = '';
+        $vendorPaths = array_slice($vendorPaths, 1); // Removing domain name
+
+        foreach ($vendorPaths as $vendorPath) {
+            $vendorName = $vendorPath . '\\' . $vendorName;
+            if ($vendorPath === $coreDomain) {
+                break;
+            }
+        }
+
+        return rtrim($vendorName, '\\');
+    }
+
+    /**
      * Loads a single domain into the manager.
      *
-     * @param string|object $namespace Eg: Kingdom\Auth
-     * @param string $path Eg: 'Kingdom/Auth';
+     * @param string|object $namespace Eg: Lms\Auth
+     * @param string $path Eg: 'LMS/Auth';
      * @return void|DomainInterface
+     *
      * @throws DomainNotExistsException
      * @throws DomainExtendException
      */
@@ -124,19 +155,16 @@ class DomainManager {
             throw new DomainNotExistsException('Domain ' . $class . ' could not be instantiated.');
         }
 
-        if(realpath($path) === false){
+        if (realpath($path) === false) {
             throw new DomainNotExistsException('Path ' . $path . ' not exists.');
         }
 
-        if(!is_subclass_of($class, DomainInterface::class)){
-            throw new DomainExtendException('The domain must extend the abstract class \Kingdom\Core\Contracts\DomainInterface');
+        if (!is_subclass_of($class, DomainInterface::class)) {
+            throw new DomainExtendException('The domain must extend the abstract class \TruckPag\Core\Contracts\DomainInterface');
         }
 
-
-        if(!is_object($class)){
-            /**
-             * @var DomainInterface $classObj
-             */
+        if (!is_object($class)) {
+            /** @var DomainInterface $class */
             $class = new $class();
         }
 
@@ -144,14 +172,13 @@ class DomainManager {
         /*
          * Check for disabled domains
          */
-        if ($class->isDisabled()){
+        if ($class->isDisabled()) {
             return;
         }
 
         $this->domains[$classId] = $class;
         $this->pathMap[$classId] = $path;
         $this->normalizedMap[strtolower($classId)] = $classId;
-
 
         return $class;
     }
@@ -162,14 +189,13 @@ class DomainManager {
      */
     private function getDomainClassName($class)
     {
-        if(is_object($class)){
+        if (is_object($class)) {
             return $class;
         }
 
-        if(is_string($class)  && !Str::endsWith($class, 'Domain')){
+        if (is_string($class) && !Str::endsWith($class, 'Domain')) {
             return $class . '\Domain';
         }
-
 
         return $class;
     }
@@ -177,21 +203,21 @@ class DomainManager {
     /**
      * Resolves a domain identifier
      *
-     * @param mixed Domain class name or object
-     * @return string Identifier in format of Domain
+     * @param mixed $namespace Domain class name or object
+     * @return string           Identifier in format of Domain
      */
     public function getIdentifier($namespace): string
     {
         $namespace = $this->normalizeClassName($namespace);
-        if (strpos($namespace, '\\') === null) {
+        if (str_contains($namespace, '\\')) {
             return $namespace;
         }
 
         $parts = explode('\\', $namespace);
         $slice = array_slice($parts, 1, 2);
+
         return implode('.', $slice);
     }
-
 
     /**
      * @return array
@@ -199,12 +225,13 @@ class DomainManager {
     public function getProviders(): array
     {
         $providers = [];
-        /** @var DomainInterface $domainObj **/
+        /** @var DomainInterface $domainObj * */
         foreach ($this->domains as $domainObj) {
-            $providers =  array_merge($providers, $domainObj->registerProvider());
+            $providers = array_merge($providers, $domainObj->registerProvider());
         }
 
         return $providers;
     }
+
 
 }
