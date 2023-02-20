2b75191c5ca16abd355c00acc4cb468dc6087438
danielhe4rt <hey@danielheart.dev>
2023-02-19 23:29:00 -0300

    wip

--- config/database.php
+++ config/database.php
@@ -58,9 +58,14 @@ return [
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-            ]) : [],
+            'sslmode' => env('DB_SSLMODE', null),
+            'options' => env('DB_SSLMODE', null) == 'required'
+                ? [
+                    PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
+                    PDO::MYSQL_ATTR_SSL_KEY => '/certs/client-key.pem',
+                    PDO::MYSQL_ATTR_SSL_CERT => '/certs/client-cert.pem',
+                    PDO::MYSQL_ATTR_SSL_CA => '/certs/ca.pem',
+                ] : []
         ],
 
         'pgsql' => [
