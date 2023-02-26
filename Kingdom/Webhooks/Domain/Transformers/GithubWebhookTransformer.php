3f16d2d82fff185dcb5dd34696f23e2fc1680dd2
danielhe4rt <hey@danielheart.dev>
2023-02-25 23:30:42 -0300

    wip

--- Kingdom/Webhooks/Domain/Transformers/GithubWebhookTransformer.php
+++ Kingdom/Webhooks/Domain/Transformers/GithubWebhookTransformer.php
@@ -9,5 +9,19 @@ class GithubWebhookTransformer implements WebhookTransformerContract
     public function toDTO(array $payload): SubscriptionWebhookDTO
     {
 
+        $transformedPayload = $this->transformPayload($payload);
+
+        return SubscriptionWebhookDTO::make($transformedPayload);
+    }
+
+    private function transformPayload(array $payload): array
+    {
+        return [
+            'status' => $payload['sender']['status'],
+            'type' => $payload['action'] ?? '',
+            'provider' => 'github',
+            'provider_id' => $payload['sender']['id'],
+            'provider_login' => $payload['sender']['login']
+        ];
     }
 }
